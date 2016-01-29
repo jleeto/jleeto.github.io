b3ea68e36d81fc3d0be2d6467862484a20f951da
jleeto <jleeto7@gmail.com>
2016-01-28 23:24:44 -0600

    update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,12 @@
-jQuery.githubUserRepositories = function(username, callback) {
-  jQuery.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
-}
-
-jQuery.fn.loadRepositores = function(username) {
-  this.html("<span>Querying GitHub for repositories...</span>");
-
-  var target = this;
-  $.githubUserRepositories(username, function(data) {
-    var repos = data.data;
-    sortByNumberOfWatchers(repos);
-
-    var list = $('<dl/>');
-    target.empty().append(list);
-    $(repos).each(function() {
-      list.append('<dt><a href="'+ this.url +'">' + this.name + '</a></dt>');
-      list.append('<dd>' + this.description + '</dd>');
+(function() {
+  $(function() {
+    $.getJSON('https://api.github.com/users/jleeto/repos?sort=pushed').success(function(repos) {
+      $('.project-repo').each(function(i) {
+        var project;
+        project = repos[i];
+        console.log(project);
+        $(this).append('<div class="title">' + '<span class="mega-octicon octicon-repo"></span>' + '<span class="name">' + project.name + '</span>' + '<span class="desc">' + project.description + '</span>' + '</div>');
+      });
     });
   });
-
-  function sortByNumberOfWatchers(repos) {
-    repos.sort(function(a,b) {
-      return b.watchers - a.watchers;
-    });
-  }
-};
+})();
