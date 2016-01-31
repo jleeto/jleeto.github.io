c7b3ddd8d7b1cff58e270db9a39a8f0dbc430522
jleeto <jleeto7@gmail.com>
2016-01-30 19:34:51 -0600

    updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,1 @@
-(function() {
-  $(function() {
-    $.getJSON('https://api.github.com/users/jleeto/repos?sort=pushed')
-    .success(function(repos) {
-      $('.project-repo').each(function(i) {
-        var project = repos[i];
-        console.log(project);
-        $(this).append(
-          '<div class="title">' +
-            '<span class="name">' + project.name + '</span>' +
-            '<span class="desc">' + project.description + '</span>' +
-          '</div>'
-        );
-      });
-    });
-  });
-})();
+
