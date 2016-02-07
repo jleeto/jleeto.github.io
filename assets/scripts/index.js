d6260ab29d88c88f933f120b59b4272d02c4836d
jleeto <jleeto7@gmail.com>
2016-02-06 20:27:49 -0600

    updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 var date = newDate();
 hour = date.getHours();
 // check for time
-if(hour > 19 || < 6) {
+if(hour > 19 || < 4) {
   night = true;
 }
 // set new background if higher than 19 o'clock
