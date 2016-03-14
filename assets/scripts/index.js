769e923473f276bc5866269ad957cd69542cc9f3
jleeto <jleeto7@gmail.com>
2016-03-13 21:27:10 -0500

    updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $.getJSON('https://mcapi.ca/query/mc.minesuperior.com/players', function(res) {
-    $('#mineplex').text(res.players.online);
+    $('#randomcount').text(res.players.online);
  });
 });
