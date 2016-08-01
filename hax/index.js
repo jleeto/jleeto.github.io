b1c0cc54cf4c9081855ad213fa7f44abff854d24
jleeto <jleeto7@gmail.com>
2016-08-01 23:06:08 +0100

    updates

--- hax/index.js
+++ hax/index.js
@@ -1,9 +1,4 @@
-    window.onload = function () {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "http://jsonip.appspot.com/?callback=DisplayIP";
-        document.getElementsByTagName("head")[0].appendChild(script);
-    };
-    function DisplayIP(response) {
-        document.getElementById("ipaddress").innerHTML = "Your IP Address is " + response.ip;
-    }
+function ipval() {
+document.write(ipval);
+}
+window.onload=ipval
