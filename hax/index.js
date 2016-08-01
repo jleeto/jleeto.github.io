ab17fdaf5d2b4b36da94265674abc754f970526a
jleeto <jleeto7@gmail.com>
2016-08-01 23:32:19 +0100

    updates

--- hax/index.js
+++ hax/index.js
@@ -1,3 +1,3 @@
 var arr = ['working on it', 'under development', 'not ready yet', 'got your ip...', 'pro haxer!232'];
 
-document.querySelector('.message').innerHTML = arr[Math.floor(Math.random() * arr.length)];2
+document.querySelector('.message').innerHTML = arr[Math.floor(Math.random() * arr.length)];
