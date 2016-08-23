5a471c8fa35150ab1d8f4b90e5265842cc5d9ec8
jleeto <jleeto7@gmail.com>
2016-08-22 22:47:45 -0500

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-document.write('test');
+$(document).ready(function() {
+  var backgrounds = [#000, #333, #111];
+
+  var randomnumber = Math.floor(Math.random() * backgrounds.length);
+  var classToAdd = backgrounds[randomnumber];
+
+  $('body').addClass(classToAdd);
+});
